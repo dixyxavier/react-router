fe4ad60d644a63a085f38a29300033dfeab26e1f
dixyxavier <jaun.dixy@razorthink.net>
2016-02-08 11:06:52 +0530

    router basics

---
+++ src/components/login.jsx
@@ -0,0 +1,13 @@
+/**
+ * Created by dixy on 4/2/16.
+ */
+import React from 'react';
+
+let Login = React.createClass({
+
+    render() {
+        return (<div>Welcome to login</div>);
+    }
+});
+
+export default Login;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'webpack/hot/only-dev-server',
+        "./src/app.jsx"
+    ],
+    output: {
+        path: __dirname + '/build',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js(x?)/,
+                exclude: /node_modules/,
+                loader: ['react-hot', 'babel-loader'],
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            }
+        ]
+    },
+    include: __dirname + 'src',
+    plugins: [
+        new webpack.NoErrorsPlugin()
+    ]
+};
